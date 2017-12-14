c74dff375fd9959b2e4e1053dadeca2d187b2424
YuliiaDear <33024352+YuliiaDear@users.noreply.github.com>
2017-12-14 15:08:46 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ var button = document.querySelector(".button-search");
 var form = document.querySelector(".search-form");
 		  
 button.addEventListener("click", function () {
-form.classList.toggle("visually-hidden");
+form.classList.toggle("popup-reveal");
 });
