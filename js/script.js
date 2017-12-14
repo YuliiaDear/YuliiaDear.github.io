d0407b12f8d277a784c89f0fe5a5ac734244f190
YuliiaDear <33024352+YuliiaDear@users.noreply.github.com>
2017-12-14 13:43:31 +0200

    третья попытка

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var button = document.querySelector(".button-search");
+var form = document.querySelector(".search-form");
+		  
+button.addEventListener("click", function () {
+form.classList.toggle("visually-hidden");
+});
