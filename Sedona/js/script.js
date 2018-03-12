4d8059a499da6ecfe39b10fed6f4ce2ac38d7cd9
YuliiaDear <33024352+YuliiaDear@users.noreply.github.com>
2018-03-12 16:41:48 +0200

    Add files via upload

---
+++ Sedona/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var button = document.querySelector(".button-search");
+var form = document.querySelector(".search-form");
+		  
+button.addEventListener("click", function () {
+form.classList.toggle("popup-reveal");
+});
